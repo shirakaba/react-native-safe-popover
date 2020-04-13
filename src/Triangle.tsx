e38a7fa20d082673acc70a7a19769e16530fd895
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-04-13 18:54:45 +0100

    Triangle converted to functional component

--- src/Triangle.tsx
+++ src/Triangle.tsx
@@ -27,7 +27,6 @@ interface TriangleProps {
      */
     height?: number
 
-
     /**
      * Fill color of triangle
      * Accepts color strings such as hex, literals, rgba
@@ -46,117 +45,112 @@ interface TriangleProps {
 /**
  * @see https://github.com/Jpoliachik/react-native-triangle
  */
-export class Triangle extends React.Component<TriangleProps, {}> {
-    public static defaultProps() {
-        return {
-            direction: 'up',
-            width: 0,
-            height: 0,
-            color: 'white',
-        };
-    }
-
-    private readonly _borderStyles = () => {
-        if (this.props.direction === 'up') {
+export function Triangle(props: TriangleProps){
+    const _borderStyles = () => {
+        if (props.direction === 'up') {
             return {
                 borderTopWidth: 0,
-                borderRightWidth: this.props.width!/2.0,
-                borderBottomWidth: this.props.height,
-                borderLeftWidth: this.props.width!/2.0,
+                borderRightWidth: props.width!/2.0,
+                borderBottomWidth: props.height,
+                borderLeftWidth: props.width!/2.0,
                 borderTopColor: 'transparent',
                 borderRightColor: 'transparent',
-                borderBottomColor: this.props.color,
+                borderBottomColor: props.color,
                 borderLeftColor: 'transparent',
             };
-        } else if (this.props.direction === 'right') {
+        } else if (props.direction === 'right') {
             return {
-                borderTopWidth: this.props.height!/2.0,
+                borderTopWidth: props.height!/2.0,
                 borderRightWidth: 0,
-                borderBottomWidth: this.props.height!/2.0,
-                borderLeftWidth: this.props.width,
+                borderBottomWidth: props.height!/2.0,
+                borderLeftWidth: props.width,
                 borderTopColor: 'transparent',
                 borderRightColor: 'transparent',
                 borderBottomColor: 'transparent',
-                borderLeftColor: this.props.color,
+                borderLeftColor: props.color,
             };
-        } else if (this.props.direction === 'down') {
+        } else if (props.direction === 'down') {
             return {
-                borderTopWidth: this.props.height,
-                borderRightWidth: this.props.width!/2.0,
+                borderTopWidth: props.height,
+                borderRightWidth: props.width!/2.0,
                 borderBottomWidth: 0,
-                borderLeftWidth: this.props.width!/2.0,
-                borderTopColor: this.props.color,
+                borderLeftWidth: props.width!/2.0,
+                borderTopColor: props.color,
                 borderRightColor: 'transparent',
                 borderBottomColor: 'transparent',
                 borderLeftColor: 'transparent',
             };
-        } else if (this.props.direction === 'left') {
+        } else if (props.direction === 'left') {
             return {
-                borderTopWidth: this.props.height!/2.0,
-                borderRightWidth: this.props.width,
-                borderBottomWidth: this.props.height!/2.0,
+                borderTopWidth: props.height!/2.0,
+                borderRightWidth: props.width,
+                borderBottomWidth: props.height!/2.0,
                 borderLeftWidth: 0,
                 borderTopColor: 'transparent',
-                borderRightColor: this.props.color,
+                borderRightColor: props.color,
                 borderBottomColor: 'transparent',
                 borderLeftColor: 'transparent',
             };
-        } else if (this.props.direction === 'up-left') {
+        } else if (props.direction === 'up-left') {
             return {
-                borderTopWidth: this.props.height,
-                borderRightWidth: this.props.width,
+                borderTopWidth: props.height,
+                borderRightWidth: props.width,
                 borderBottomWidth: 0,
                 borderLeftWidth: 0,
-                borderTopColor: this.props.color,
+                borderTopColor: props.color,
                 borderRightColor: 'transparent',
                 borderBottomColor: 'transparent',
                 borderLeftColor: 'transparent',
             };
-        } else if (this.props.direction === 'up-right') {
+        } else if (props.direction === 'up-right') {
             return {
                 borderTopWidth: 0,
-                borderRightWidth: this.props.width,
-                borderBottomWidth: this.props.height,
+                borderRightWidth: props.width,
+                borderBottomWidth: props.height,
                 borderLeftWidth: 0,
                 borderTopColor: 'transparent',
-                borderRightColor: this.props.color,
+                borderRightColor: props.color,
                 borderBottomColor: 'transparent',
                 borderLeftColor: 'transparent',
             };
-        } else if (this.props.direction === 'down-left') {
+        } else if (props.direction === 'down-left') {
             return {
-                borderTopWidth: this.props.height,
+                borderTopWidth: props.height,
                 borderRightWidth: 0,
                 borderBottomWidth: 0,
-                borderLeftWidth: this.props.width,
+                borderLeftWidth: props.width,
                 borderTopColor: 'transparent',
                 borderRightColor: 'transparent',
                 borderBottomColor: 'transparent',
-                borderLeftColor: this.props.color,
+                borderLeftColor: props.color,
             };
-        } else if (this.props.direction === 'down-right') {
+        } else if (props.direction === 'down-right') {
             return {
                 borderTopWidth: 0,
                 borderRightWidth: 0,
-                borderBottomWidth: this.props.height,
-                borderLeftWidth: this.props.width,
+                borderBottomWidth: props.height,
+                borderLeftWidth: props.width,
                 borderTopColor: 'transparent',
                 borderRightColor: 'transparent',
-                borderBottomColor: this.props.color,
+                borderBottomColor: props.color,
                 borderLeftColor: 'transparent',
             };
         } else {
-            console.error('Triangle.js wrong direction. ' + this.props.direction + ' is invalid. Must be one of: ' + ['up', 'right', 'down', 'left', 'up-right', 'up-left', 'down-right', 'down-left']);
+            console.error('Triangle.js wrong direction. ' + props.direction + ' is invalid. Must be one of: ' + ['up', 'right', 'down', 'left', 'up-right', 'up-left', 'down-right', 'down-left']);
             return {};
         }
     }
 
-    public render() {
-        const borderStyles = this._borderStyles();
-        return (
-            <View style={[styles.triangle, borderStyles, this.props.style]}/>
-        )
-    }
+    const borderStyles = _borderStyles();
+    return (
+        <View style={[styles.triangle, borderStyles, props.style]}/>
+    );
+};
+Triangle.defaultProps = {
+    direction: 'up',
+    width: 0,
+    height: 0,
+    color: 'white',
 };
 
 const styles = StyleSheet.create({
